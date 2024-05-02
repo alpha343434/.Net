98eac3fe7e326d2ed4eb23c593095e19651b13a7
Zehra <130374883+alpha343434@users.noreply.github.com>
2024-05-02 16:22:45 +0300

    Add files via upload

---
+++ C#/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+
+        public string CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnistinStock { get; set; }
+        public int UnitPrice { get; set; }
+
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,33 @@
+﻿// our customers:
+
+using Entities;
+
+int number = 0;
+
+Customer customer1 = new RealCustomer();    
+customer1.Name = "Test";
+
+Customer customer2 = new RealCustomer();
+customer2.Name = "Test";
+
+
+
+List<Customer> customers = new List<Customer>();
+
+
+AddCustomer(customer1, customers);
+AddCustomer(customer2, customers);
+
+void AddRealCustomer(RealCustomer realCustomer, List<RealCustomer> realcustomers)
+{
+    realcustomers.Add(realCustomer);
+}
+void AddCustomer(Customer customer, List<Customer> customers)
+{
+    customers.Add(customer);
+}
+
+foreach (Customer customer in customers)
+{
+    Console.WriteLine($" {customer.Name}");
+}
