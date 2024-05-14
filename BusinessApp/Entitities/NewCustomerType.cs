a790056fe451c73c30f0555210da7cb699deaa75
Zehra <130374883+alpha343434@users.noreply.github.com>
2024-05-14 10:23:44 +0300

    Add files via upload

---
+++ BusinessApp/Entitities/NewCustomerType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class NewCustomerType : Customer
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string CustomerNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
+
